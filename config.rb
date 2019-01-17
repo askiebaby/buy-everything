317f4a2b295b6cdeae3b3c5d414cb4ed1fe490a6
Askie Lin <isme408@gmail.com>
2019-01-17 16:43:10 +0800

    https test for login response

---
+++ config.rb
@@ -0,0 +1,26 @@
+
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"           # 你的根目錄位置
+css_dir = "style"           # 你的CSS資料夾路徑
+sass_dir = "sass"         # 你的Sass資料夾路徑
+images_dir = "images"     # 你的圖片資料夾路徑
+javascripts_dir = "js"    # 你的js資料夾路徑
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
