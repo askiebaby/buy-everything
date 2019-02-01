37d20bcd4066840a8fae0ec193021bed256a4d58
Askie Lin <isme408@gmail.com>
2019-02-01 14:39:03 +0800

    整理 function

--- js/login-ajax.js
+++ js/login-ajax.js
@@ -1,33 +1,41 @@
 $( document ).ready(function() {
 
-
+  // 初始畫面
+  let userToken = Cookies.get('buy-user-token')
   let fbButton = document.querySelector('.login-button')
-  let userToken = Cookies.get('buy-user-token');
   let loginPageFunc = document.querySelector('.loginPage__login__func')
-  fbButton.innerHTML = '登入'
+  fbButton.innerHTML = '確認登入狀態中...'
   
+  // 檢驗 token 是否存在
+  // => 存在則送驗
+  // ====> 有效 => 載入使用者狀態及可使用的功能
+  // ====> 無效 => 背景重新存取 token
+  // => 不存在需要使用者登入及授權
+
   if(userToken) {
-    // 驗 token 是否有效
-    api_user(userToken, '');
+    api_user(userToken, '')
   } else {
-    fbSDK();
+    fbSDK()
+    fbButton.addEventListener('click', buttonInit)
   }
 
-  fbButton.addEventListener('click', buttonInit)
+
+  // 登入：賦予按鈕登入事件
 
   function buttonInit () {
-    // callback
     FB.login(function(response) {
       if (response.authResponse) {
-          statusChangeCallback(response);
+          statusChangeCallback(response)
       } else {
-        console.log('User cancelled login or did not fully authorize.');
+        console.log('User cancelled login or did not fully authorize.')
       }
     });
   }
 
+
+  // 登出
+
   function buttonLogout () {
-    console.log(userToken)
     FB.init({
       appId  : '326735094614431',
       xfbml  : true,
@@ -41,61 +49,59 @@ $( document ).ready(function() {
       fbButton.innerHTML = '登入'
       fbButton.removeEventListener('click', buttonLogout)
       fbButton.addEventListener('click', buttonInit)
-      Cookies.remove('buy-user-token');
-    });
+      Cookies.remove('buy-user-token')
+    })
   }
 
   // 首先先確定 cookie 在不在
+  
   $.ajax({
       url: '//cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js',
       dataType: 'script',
       success: function() {
         
         // 驗 token 是否存在
-        let userToken = Cookies.get('buy-user-token');
-        console.log(userToken)
+        let userToken = Cookies.get('buy-user-token')
 
         // 驗 token 是否有效
         if(userToken) {
-          api_user(userToken, '');
+          api_user(userToken, '')
         }else{
-          fbSDK();
+          fbSDK()
         }
       },
       error: function(){
-        console.log('user 檢驗錯誤');
+        console.log('user 檢驗錯誤')
       }
-  });
+  })
 
 
   // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
-    console.log('step 4: statusChangeCallback is called!', response);
+    console.log('step 4: statusChangeCallback is called!', response)
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
-      console.log('step 4.1: statusChangeCallback: connected');
+      console.log('step 4.1: statusChangeCallback: connected')
       // Logged into your app and Facebook.
       // cookies
-      let userToken = response.authResponse.accessToken;
-      let userTokenExpiresIn = response.authResponse.expiresIn;
-      let userTokenExpiresInString = JSON.stringify({'expiresIn': userTokenExpiresIn});
+      let userToken = response.authResponse.accessToken
+      let userTokenExpiresIn = response.authResponse.expiresIn
+      let userTokenExpiresInString = JSON.stringify({'expiresIn': userTokenExpiresIn})
 
-      saveToken(response.authResponse.accessToken);
-      api_token(userToken, userTokenExpiresInString);
+      saveToken(response.authResponse.accessToken)
+      api_token(userToken, userTokenExpiresInString)
     } 
     else {
-      console.log('345hj')
+      // The person is not logged into your app or we are unable to tell.
+
       fbButton.innerHTML = '登入'
       fbButton.removeEventListener('click', buttonLogout)
       fbButton.addEventListener('click', buttonInit)
       loginPageFunc.innerHTML = ''
-      // The person is not logged into your app or we are unable to tell.
-      // location.reload()
-      // document.getElementById('status').innerHTML = 'Please log ' +
-        // 'into this app.';
+
     }
   }
 
@@ -103,11 +109,10 @@ $( document ).ready(function() {
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
   function checkLoginState() {
-    console.log('step 3: 確認使用者登入狀態');
+    console.log('step 3: 確認使用者登入狀態')
     FB.getLoginStatus(function(response) {
-      // statusChangeCallback(response);
-      // console.log(response)
-    });
+      statusChangeCallback(response)
+    })
   }
 
   function fbSDK (){
@@ -119,22 +124,22 @@ $( document ).ready(function() {
       dataType: 'script',
       cache: true,
       success:function() {
-        console.log('step 1: 確保 SDK 已經載入完成');
+        console.log('step 1: 確保 SDK 已經載入完成')
         FB.init({
           appId  : '326735094614431',
           xfbml  : true,
           version: 'v2.8'
         },
-        console.log('step 2: FB 物件存在，所以初始化'));
-        checkLoginState();
+        console.log('step 2: FB 物件存在，所以初始化'))
+        checkLoginState()
       }
-    });
+    })
   }
 
 
   function saveToken (fbToken) {
     Cookies.set('buy-user-token', fbToken);
-    console.log('拿到使用者token，存到 cookie中');
+    console.log('拿到使用者token，存到 cookie中')
   }
 
 
@@ -155,15 +160,15 @@ $( document ).ready(function() {
     
     $.ajax(settings).done(function (response) {
       if (response.result ===  true) {
-        console.log('token', response);
+        console.log('token', response)
         // 執行 get.user data
-        api_user(userToken, '');
+        api_user(userToken, '')
       }
-    });
+    })
 
     $.ajax(settings).fail(function (jqXHR, textStatus, errorThrown) {
-      console.log('userToken: ' + errorThrown);
-    });
+      console.log('userToken: ' + errorThrown)
+    })
   }
 
   function api_user(userToken, data){
@@ -178,7 +183,6 @@ $( document ).ready(function() {
       },
       'data': data
     }
-    console.log(settings);
 
     $.ajax(settings).done(function (response) {
       if (response.result === true) {
@@ -189,13 +193,13 @@ $( document ).ready(function() {
         userLoginSuccess(userName, userPhoto);
       }
       
-    });
+    })
 
     $.ajax(settings).fail(function (response) {
-      console.log('api_user: Fail ', response);
-      fbButton.innerHTML = '登入'
-      fbSDK();
-    });
+      console.log('api_user: Fail ', response)
+      fbButton.innerHTML = '登入' // 未登入的初始畫面
+      fbSDK()
+    })
   }
 
   function userLoginSuccess(userName, userPhoto){
